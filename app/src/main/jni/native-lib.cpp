b544fc0bcdcba35e17154b7269dfd58272dfe451
Atamg1994 <57526178+Atamg1994@users.noreply.github.com>
2026-02-13 00:02:53 +0200

    Update native-lib.cpp
    Add load another lib
    If extis "Load" by name

--- app/src/main/jni/native-lib.cpp
+++ app/src/main/jni/native-lib.cpp
@@ -3,10 +3,66 @@
 #include <thread>
 #include <dlfcn.h>
 #include <unistd.h>
+#include <dirent.h>
 #include <sys/system_properties.h>
 #include "Il2Cpp/il2cpp_dump.h"
 #include "Includes/config.h"
 #include "Includes/log.h"
+
+// Функция для поиска и загрузки сторонних библиотек с "Load" в имени
+void loadExtraLibraries() {
+    // Путь к нативным библиотекам приложения
+    // В большинстве случаев это /data/data/название.пакета/lib или путь через /proc/self/exe
+    // Но самый надежный способ для инжекта - прочитать свою папку из /proc/self/maps
+    
+    char line[512];
+    std::string libDir = "";
+    FILE *fp = fopen("/proc/self/maps", "rt");
+    if (fp) {
+        while (fgets(line, sizeof(line), fp)) {
+            if (strstr(line, "/lib/") && strstr(line, ".so")) {
+                std::string path = line;
+                size_t start = path.find("/");
+                size_t end = path.find_last_of("/");
+                if (start != std::string::npos && end != std::string::npos) {
+                    libDir = path.substr(start, end - start);
+                    break;
+                }
+            }
+        }
+        fclose(fp);
+    }
+
+    if (libDir.empty()) {
+        LOGE("Could not find library directory");
+        return;
+    }
+
+    LOGI("Scanning directory for 'Load' libraries: %s", libDir.c_str());
+
+    DIR* dir = opendir(libDir.c_str());
+    if (dir) {
+        struct dirent* entry;
+        while ((entry = readdir(dir)) != nullptr) {
+            // Ищем "Load" в имени и проверяем что это .so
+            if (strstr(entry->d_name, "Load") && strstr(entry->d_name, ".so")) {
+                // Игнорируем саму себя, если вдруг в нашем имени есть "Load"
+                if (strstr(entry->d_name, "native-lib")) continue; 
+
+                std::string fullPath = libDir + "/" + entry->d_name;
+                LOGI("Found extra library: %s", entry->d_name);
+                
+                void* handle = dlopen(fullPath.c_str(), RTLD_NOW);
+                if (handle) {
+                    LOGI("Successfully loaded: %s", entry->d_name);
+                } else {
+                    LOGE("Failed to load %s: %s", entry->d_name, dlerror());
+                }
+            }
+        }
+        closedir(dir);
+    }
+}
 
 bool isLibraryLoaded(const char *libraryName) {
     char line[512] = {0};
@@ -14,6 +70,7 @@ bool isLibraryLoaded(const char *libraryName) {
     if (fp != nullptr) {
         while (fgets(line, sizeof(line), fp)) {
             if (strstr(line, libraryName)) {
+                fclose(fp);
                 return true;
             }
         }
@@ -26,11 +83,14 @@ bool isLibraryLoaded(const char *libraryName) {
 
 void dump_thread() {
     LOGI("Lib loaded");
+    
+    // Сначала загружаем доп. библиотеки, если они есть
+    loadExtraLibraries();
+
     do {
         sleep(1);
     } while (!isLibraryLoaded(libTarget));
 
-    //Waiting libil2cpp.so fully loaded.
     LOGI("Waiting in %d...", Sleep);
     sleep(Sleep);
 
@@ -44,15 +104,9 @@ void dump_thread() {
     il2cpp_dump(androidDataPath.c_str());
 }
 
-//The idea from first Il2Cpp Dumper called PokemonGoDumper
-//https://github.com/Jumboperson/PokemonGoDumper/blob/master/main.c#L569
-
 void *pLibRealUnity = 0;
-
 typedef jint(JNICALL *CallJNI_OnLoad_t)(JavaVM *vm, void *reserved);
-
 typedef void(JNICALL *CallJNI_OnUnload_t)(JavaVM *vm, void *reserved);
-
 CallJNI_OnLoad_t RealJNIOnLoad = 0;
 CallJNI_OnUnload_t RealJNIOnUnload = 0;
 
@@ -60,40 +114,33 @@ CallJNI_OnUnload_t RealJNIOnUnload = 0;
 
 JNIEXPORT jint JNICALL CallJNIOL(JavaVM *vm, void *reserved) {
     LOGI("OnLoad called");
-
     std::thread(dump_thread).detach();
 
-    if (!pLibRealUnity)
-        pLibRealUnity = dlopen("librealmain.so", RTLD_NOW);
-    if (!pLibRealUnity)
-        pLibRealUnity = dlopen("librealunity.so", RTLD_NOW);
-    if (!RealJNIOnLoad)
+    if (!pLibRealUnity) pLibRealUnity = dlopen("librealmain.so", RTLD_NOW);
+    if (!pLibRealUnity) pLibRealUnity = dlopen("librealunity.so", RTLD_NOW);
+    
+    if (pLibRealUnity && !RealJNIOnLoad) {
         RealJNIOnLoad = reinterpret_cast<CallJNI_OnLoad_t>(dlsym(pLibRealUnity, "JNI_OnLoad"));
-    return RealJNIOnLoad(vm, reserved);
+    }
+    
+    return (RealJNIOnLoad) ? RealJNIOnLoad(vm, reserved) : JNI_VERSION_1_6;
 }
 
 JNIEXPORT void JNICALL CallJNIUL(JavaVM *vm, void *reserved) {
     LOGI("OnUnload called");
-
-    if (!pLibRealUnity)
-        pLibRealUnity = dlopen("librealmain.so", RTLD_NOW);
-    if (!pLibRealUnity)
-        pLibRealUnity = dlopen("librealunity.so", RTLD_NOW);
-    if (!RealJNIOnUnload)
-        RealJNIOnUnload = reinterpret_cast<CallJNI_OnUnload_t>(dlsym(pLibRealUnity,
-                                                                     "JNI_OnUnload"));
-    RealJNIOnUnload(vm, reserved);
+    if (pLibRealUnity && !RealJNIOnUnload) {
+        RealJNIOnUnload = reinterpret_cast<CallJNI_OnUnload_t>(dlsym(pLibRealUnity, "JNI_OnUnload"));
+    }
+    if (RealJNIOnUnload) RealJNIOnUnload(vm, reserved);
 }
 
 JNIEXPORT jint JNICALL JNI_OnLoad(JavaVM *vm, void *reserved) {
     LOGI("Initialize JNI");
-
     return CallJNIOL(vm, reserved);
 }
 
 JNIEXPORT void JNICALL JNI_OnUnload(JavaVM *vm, void *reserved) {
     LOGI("Unload JNI");
-
     CallJNIUL(vm, reserved);
 }
 
@@ -101,7 +148,6 @@ JNIEXPORT void JNICALL JNI_OnUnload(JavaVM *vm, void *reserved) {
 
 __attribute__((constructor))
 void lib_main() {
-    // Create a new thread so it does not block the main thread, means the game would not freeze
     std::thread(dump_thread).detach();
 }
 #endif
