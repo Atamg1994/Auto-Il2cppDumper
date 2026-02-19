7813d2d9a9b205e5d3e48fbdd839622982b5da61
Atamg1994 <57526178+Atamg1994@users.noreply.github.com>
2026-02-19 03:22:01 +0200

    Update Android.mk

--- app/src/main/jni/Android.mk
+++ app/src/main/jni/Android.mk
@@ -4,29 +4,31 @@ include $(CLEAR_VARS)
 
 LOCAL_MODULE    := ModLoader
 
-# Базовые флаги
-LOCAL_CFLAGS := -Wno-error=format-security -fvisibility=hidden -ffunction-sections -fdata-sections -w
-LOCAL_CFLAGS += -fno-rtti -fno-exceptions -fpermissive
+# Базовые флаги (убрали -fno-rtti и -fno-exceptions)
+LOCAL_CFLAGS := -Wno-error=format-security -fvisibility=hidden -ffunction-sections -fdata-sections -w -fpermissive
 
-# ФЛАГИ ДЛЯ ПОДДЕРЖКИ JNI-BIND (C++20 + РАСШИРЕНИЯ CLANG)
-LOCAL_CPPFLAGS := -Wno-error=format-security -fvisibility=hidden -ffunction-sections -fdata-sections -w -s -std=c++20
-LOCAL_CPPFLAGS += -fno-rtti -fno-exceptions -fpermissive
-LOCAL_CPPFLAGS += -frelaxed-template-template-args
-LOCAL_CPPFLAGS += -DNI_BIND_MAX_ARG_COUNT=10
+# ФЛАГИ ДЛЯ ПОДДЕРЖКИ JNI-BIND (ВКЛЮЧАЕМ RTTI И ИСКЛЮЧЕНИЯ)
+LOCAL_CPPFLAGS := -std=c++20 -frtti -fexceptions \
+                  -Wno-error=format-security -fvisibility=hidden \
+                  -ffunction-sections -fdata-sections -w -fpermissive \
+                  -frelaxed-template-template-args \
+                  -DJNI_BIND_MAX_ARG_COUNT=10
 
 LOCAL_LDFLAGS += -Wl,--gc-sections,--strip-all -llog
 LOCAL_ARM_MODE := arm
 
-LOCAL_C_INCLUDES += $(MAIN_LOCAL_PATH)
-LOCAL_C_INCLUDES += $(LOCAL_PATH)
-LOCAL_C_INCLUDES += $(LOCAL_PATH)/Includes
-LOCAL_C_INCLUDES += $(LOCAL_PATH)/Il2Cpp/xdl/include
+LOCAL_C_INCLUDES += $(MAIN_LOCAL_PATH) \
+                    $(LOCAL_PATH) \
+                    $(LOCAL_PATH)/Includes \
+                    $(LOCAL_PATH)/Il2Cpp/xdl/include
 
+# Получаем список файлов xdl
 FILE_LIST := $(wildcard $(LOCAL_PATH)/Il2Cpp/xdl/*.c)
 
-LOCAL_SRC_FILES := $(FILE_LIST:$(LOCAL_PATH)/%=%)
-LOCAL_SRC_FILES += native-lib.cpp \
+LOCAL_SRC_FILES := $(FILE_LIST:$(LOCAL_PATH)/%=%) \
+                   native-lib.cpp \
                    Il2Cpp/il2cpp_dump.cpp
 
 LOCAL_LDLIBS := -llog -landroid -lGLESv2 -ldl
+
 include $(BUILD_SHARED_LIBRARY)
