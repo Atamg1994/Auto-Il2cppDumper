1578cbbdbfd78d624600f143e0ef5c27a3c91ce3
Atamg1994 <57526178+Atamg1994@users.noreply.github.com>
2026-02-19 02:50:10 +0200

    Update Android.mk

--- app/src/main/jni/Android.mk
+++ app/src/main/jni/Android.mk
@@ -2,17 +2,24 @@ LOCAL_PATH := $(call my-dir)
 MAIN_LOCAL_PATH := $(call my-dir)
 include $(CLEAR_VARS)
 
-LOCAL_MODULE    := il2cppdumper
+LOCAL_MODULE    := ModLoader
 
+# Базовые флаги
 LOCAL_CFLAGS := -Wno-error=format-security -fvisibility=hidden -ffunction-sections -fdata-sections -w
 LOCAL_CFLAGS += -fno-rtti -fno-exceptions -fpermissive
-LOCAL_CPPFLAGS := -Wno-error=format-security -fvisibility=hidden -ffunction-sections -fdata-sections -w -Werror -s -std=c++20
-LOCAL_CPPFLAGS += -Wno-error=c++11-narrowing -fms-extensions -fno-rtti -fno-exceptions -fpermissive
-LOCAL_LDFLAGS += -Wl,--gc-sections,--strip-all, -llog
+
+# ФЛАГИ ДЛЯ ПОДДЕРЖКИ JNI-BIND (C++20 + РАСШИРЕНИЯ CLANG)
+LOCAL_CPPFLAGS := -Wno-error=format-security -fvisibility=hidden -ffunction-sections -fdata-sections -w -s -std=c++20
+LOCAL_CPPFLAGS += -fno-rtti -fno-exceptions -fpermissive
+LOCAL_CPPFLAGS += -frelaxed-template-template-args
+LOCAL_CPPFLAGS += -DNI_BIND_MAX_ARG_COUNT=10
+
+LOCAL_LDFLAGS += -Wl,--gc-sections,--strip-all -llog
 LOCAL_ARM_MODE := arm
 
 LOCAL_C_INCLUDES += $(MAIN_LOCAL_PATH)
 LOCAL_C_INCLUDES += $(LOCAL_PATH)
+LOCAL_C_INCLUDES += $(LOCAL_PATH)/Includes
 LOCAL_C_INCLUDES += $(LOCAL_PATH)/Il2Cpp/xdl/include
 
 FILE_LIST := $(wildcard $(LOCAL_PATH)/Il2Cpp/xdl/*.c)
