d323466fbe41d2abd647030342e10ec03852cc17
Atamg1994 <57526178+Atamg1994@users.noreply.github.com>
2026-02-24 01:10:38 +0200

    Modify Android.mk for C++20 support and optimizations
    Updated compiler flags to support C++20 and added -D_GNU_SOURCE for RTLD_NEXT functionality.

--- app/src/main/jni/Android.mk
+++ app/src/main/jni/Android.mk
@@ -4,31 +4,39 @@ include $(CLEAR_VARS)
 
 LOCAL_MODULE    := ModLoader
 
-# Базовые флаги (убрали -fno-rtti и -fno-exceptions)
-LOCAL_CFLAGS := -Wno-error=format-security -fvisibility=hidden -ffunction-sections -fdata-sections -w -fpermissive
+# Добавили -D_GNU_SOURCE для работы RTLD_NEXT и хуков
+LOCAL_CFLAGS := -Wno-error=format-security -fvisibility=hidden \
+                -ffunction-sections -fdata-sections -w -fpermissive \
+                -D_GNU_SOURCE
 
-# ФЛАГИ ДЛЯ ПОДДЕРЖКИ JNI-BIND (ВКЛЮЧАЕМ RTTI И ИСКЛЮЧЕНИЯ)
+# Добавили флаги для C++20 и поддержки JNI-BIND
 LOCAL_CPPFLAGS := -std=c++20 -frtti -fexceptions \
                   -Wno-error=format-security -fvisibility=hidden \
                   -ffunction-sections -fdata-sections -w -fpermissive \
                   -frelaxed-template-template-args \
-                  -DJNI_BIND_MAX_ARG_COUNT=10
+                  -DJNI_BIND_MAX_ARG_COUNT=10 \
+                  -D_GNU_SOURCE
 
-LOCAL_LDFLAGS += -Wl,--gc-sections,--strip-all -llog
+# Оптимизация размера и удаление лишних секций
+LOCAL_LDFLAGS += -Wl,--gc-sections,--strip-all
+
 LOCAL_ARM_MODE := arm
 
+# Пути к инклудам
 LOCAL_C_INCLUDES += $(MAIN_LOCAL_PATH) \
                     $(LOCAL_PATH) \
                     $(LOCAL_PATH)/Includes \
                     $(LOCAL_PATH)/Il2Cpp/xdl/include
 
-# Получаем список файлов xdl
+# Автоматический поиск всех .c файлов в папке xdl
 FILE_LIST := $(wildcard $(LOCAL_PATH)/Il2Cpp/xdl/*.c)
 
+# Формируем список исходников
 LOCAL_SRC_FILES := $(FILE_LIST:$(LOCAL_PATH)/%=%) \
                    native-lib.cpp \
                    Il2Cpp/il2cpp_dump.cpp
 
+# Библиотеки для линковки
 LOCAL_LDLIBS := -llog -landroid -lGLESv2 -ldl
 
 include $(BUILD_SHARED_LIBRARY)
